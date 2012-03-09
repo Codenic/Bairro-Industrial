af5610ab7a82fb8f289f47382c7e9e0d6fa3a37a
Matheus Lima <matheus.se@gmail.com>
2012-03-08 23:10:47 -0300

    Frist commit

---
+++ templates/business3/blocks/footer.php
@@ -0,0 +1,21 @@
+<?php
+/*
+# ------------------------------------------------------------------------
+# JA T3 System plugin for Joomla 1.6
+# ------------------------------------------------------------------------
+# Copyright (C) 2004-2010 JoomlArt.com. All Rights Reserved.
+# @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+# Author: JoomlArt.com
+# Websites: http://www.joomlart.com - http://www.joomlancers.com.
+# ------------------------------------------------------------------------
+*/
+?>
+<div class="ja-copyright">
+	<jdoc:include type="modules" name="footer" />
+</div>
+
+
+<div class="ja-footnav">
+	<jdoc:include type="modules" name="footnav" />
+</div>
+
